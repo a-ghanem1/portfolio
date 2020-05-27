04e1dcb5e25c067f3f470dd3aab09e7e8b8ec7c2
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-05-27 14:20:07 +0200

    Integrate technologies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_003145) do
+ActiveRecord::Schema.define(version: 2020_05_27_115353) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 2020_05_25_003145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "badge"
+  end
+
+  create_table "technologies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
   end
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
