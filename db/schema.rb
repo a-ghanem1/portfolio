2685846e5ac4d79e7ceabb2552e8885b66edd832
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-06-02 13:53:27 +0200

    Added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_180501) do
+ActiveRecord::Schema.define(version: 2020_06_02_115135) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_180501) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
