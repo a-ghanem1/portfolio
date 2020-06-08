006299d4da5e65c28ebd39923bc67fb08e218e0e
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-06-09 01:03:51 +0200

    Created a comment broadcast, configured the websocket route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register', sign_out: 'logout' }
   root to: 'pages#home'
   
@@ -17,4 +16,6 @@ Rails.application.routes.draw do
   		get :toggle_status
   	end
   end
+
+  mount ActionCable.server => '/cable'
 end
